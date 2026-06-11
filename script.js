6c581ec26d4ad45911123095197acbc30f3f0178
TejasZalte <tejaszalte1383@gmail.com>
2026-06-11 08:56:41 +0530

    First Website Upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("btn").addEventListener("click", function () {
+
+    document.getElementById("message").innerText =
+        "Congratulations! JavaScript is working.";
+
+});
